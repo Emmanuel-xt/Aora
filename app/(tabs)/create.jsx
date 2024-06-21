b4c69a57496a262d7ce46009df1c99bbf7880687
Emmanuel-xt <fattmane5@gmail.com>
2024-06-21 23:49:34 +0100

    create page begin

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import FormField from '../../components/FormField'
 
 const Create = () => {
   return (
     <View>
-      <Text>Create</Text>
+      <FormField/>
     </View>
   )
 }
